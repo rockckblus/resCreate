0bca8bafaacbc7effc9a61b3b6b2c77b3a114889
rockblus <89369750@qq.com>
2016-02-25 12:45:17 +0800

    冲突测试

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function (grunt) {
         },
 
 
-        /** angular 模板合并  */
+        /** angular 模板合并  冲突测试 */
         ngtemplates: {
             dipan: {
                 cwd: '../../',
